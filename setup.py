488e2bcca01f12b90a270ed8eab5bce7b9fc1a82
Chris Pappalardo <cpappala@gmail.com>
2018-04-08 14:44:28 -0700

    updated setup and package config

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
+'''
+djcorecap/setup
+---------------
 
-from setuptools import setup, find_packages
+setuptools setup file for the djcorecap package
+'''
+
+
+import os
+from setuptools import find_packages, setup
+
 
 with open('README') as readme_file:
     readme = readme_file.read()
@@ -11,40 +18,42 @@ with open('README') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'django>=1.8',
+]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
+
 
 setup(
-    author="Chris Pappalardo",
+    author='Chris Pappalardo',
     author_email='cpappala@gmail.com',
     classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    description="Core app for Django projects that provides essential functions, tags, and enhanced Bootstrap-friendly templates.",
-    entry_points={
-        'console_scripts': [
-            'djcorecap=djcorecap.cli:entry_point',
-        ],
-    },
+    description='Core app for Django projects that provides essential functions, tags, and enhanced Bootstrap-friendly templates.',
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description='%s\n\n%s' % (readme, history),
     include_package_data=True,
     keywords='djcorecap',
     name='djcorecap',
-    packages=find_packages(include=['djcorecap']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
