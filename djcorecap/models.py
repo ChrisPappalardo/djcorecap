3abc13a942b8fa73de78b915980164d6d9824332
Chris Pappalardo <cpappala@gmail.com>
2018-10-21 10:31:17 -0700

    added SqlExecuteDict to models module

--- djcorecap/models.py
+++ djcorecap/models.py
@@ -1,5 +1,28 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
 
-from django.db import models
+'''
+djcorecap/models
+----------------
 
-# Create your models here.
+django model and manager helper functions
+'''
+
+from django.db import connection
+
+
+class SqlExecuteDict(object):
+    '''
+    mixin for raw SQL queries returning a dict
+    '''
+
+    def query_dict(self, query, *args):
+        '''
+        execute raw sql and return list of dicts
+        '''
+
+        with connection.cursor() as cursor:
+
+            cursor.execute(query, params=args)
+
+            columns = [c[0] for c in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
