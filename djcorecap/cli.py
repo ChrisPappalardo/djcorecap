ccc84de901a65405e1c1a21f6850dede501395f6
Chris Pappalardo <cpappala@gmail.com>
2019-04-08 09:02:10 -0700

    added since function to cli module ; updated requirements and docker config for unit testing

--- djcorecap/cli.py
+++ djcorecap/cli.py
@@ -8,8 +8,54 @@ console script for djcorecap.
 '''
 
 
+import collections
 import click
+import datetime as dt
+import re
 import sys
+
+from django.utils import timezone
+
+
+def since(since, start=None):
+    '''
+    calculates starting date using a list of "since" strings
+    '''
+
+    if not isinstance(since, collections.abc.Sequence):
+        raise TypeError('since arg must be a list or sequence of strings')
+
+    start = timezone.now() if start is None else start
+
+    for s in since:
+
+        hours = re.search('(\d+?)h', s)
+        days = re.search('(\d+?)d', s)
+        months = re.search('(\d+?)m', s)
+        years = re.search('(\d+?)y', s)
+
+        if hours:
+            start -= dt.timedelta(hours=int(hours[1]))
+
+        if days:
+            start -= dt.timedelta(days=int(days[1]))
+
+        if months:
+            for i in range(0, int(months[1])):
+                start = dt.datetime(
+                    year=start.year - 1 if start.month == 1 else start.year,
+                    month=start.month - 1 if start.month > 1 else 12,
+                    day=start.day,
+                )
+
+        if years:
+            start = dt.datetime(
+                year=start.year-1,
+                month=start.month,
+                day=start.day,
+            )
+
+    return start
 
 
 @click.command()
--- tests/test.py
+++ tests/test.py
@@ -8,12 +8,10 @@ test
 unit tests for djcorecap package
 '''
 
-
-
-import unittest
 from click.testing import CliRunner
+import datetime as dt
+import unittest
 
-from djcorecap import djcorecap
 from djcorecap import cli
 
 
@@ -25,8 +23,17 @@ class TestDjcorecap(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        self.assertTrue(True)
+    def test_since(self):
+        start = dt.datetime(year=2019, month=1, day=1, hour=23, minute=59, second=59)
+        since_1h = cli.since(['1h'], start)
+        since_1d = cli.since(['1d'], start)
+        since_1m = cli.since(['1m'], start)
+        since_1y = cli.since(['1y'], start)
+        self.assertIsInstance(since_1h, dt.datetime)
+        self.assertEqual(since_1h.hour, 22)
+        self.assertEqual(since_1d.day, 31)
+        self.assertEqual(since_1m.month, 12)
+        self.assertEqual(since_1y.year, 2018)
 
     def test_command_line_interface(self):
         result = self.runner.invoke(cli.main)
