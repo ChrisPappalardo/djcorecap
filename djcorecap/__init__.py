da4f6fec40cfea1cca6915f81179a3d3b5e791e7
Chris Pappalardo <cpappala@gmail.com>
2020-04-10 19:33:08 +0000

    minimized install requirements

--- djcorecap/__init__.py
+++ djcorecap/__init__.py
@@ -6,4 +6,4 @@ Top-level package for djcorecap.
 
 __author__ = 'Chris Pappalardo'
 __email__ = 'cpappala@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
--- setup.py
+++ setup.py
@@ -19,9 +19,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'bokeh>=1.1.0',
-    'django>=1.8',
-    'django-crispy-forms>=1.7.2',
+    'django',
 ]
 
 setup_requirements = []
@@ -62,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ChrisPappalardo/djcorecap',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
